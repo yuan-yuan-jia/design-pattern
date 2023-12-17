e20942c117c0204ffb84494bbe70040b8a55c636
jyy <1305081045@qq.com>
2023-12-17 08:52:52 +0800

    ch10-模板模式-完成版

---
+++ src/main/java/com/jyy/template/ConcreteClassB.java
@@ -0,0 +1,13 @@
+package com.jyy.template;
+
+public class ConcreteClassB extends AbstractClass{
+    @Override
+    public void primitiveOperation1() {
+        System.out.println("具体类B方法1实现");
+    }
+
+    @Override
+    public void primitiveOperation2() {
+        System.out.println("具体类B方法2实现");
+    }
+}
--- src/main/java/com/jyy/template/TestPaperB.java
+++ src/main/java/com/jyy/template/TestPaperB.java
@@ -1,22 +1,19 @@
 package com.jyy.template;
 
-public class TestPaperB extends TestPaper{
+public class TestPaperB extends TestPaper {
 
     @Override
-    public void testQuestion1() {
-        super.testQuestion1();
-        System.out.println("答案:d");
+    protected String answer1() {
+        return "d";
     }
 
     @Override
-    public void testQuestion2() {
-        super.testQuestion2();
-        System.out.println("答案: b");
+    protected String answer2() {
+        return "b";
     }
 
     @Override
-    public void testQuestion3() {
-        super.testQuestion3();
-        System.out.println("答案: a");
+    protected String answer3() {
+        return "a";
     }
 }
